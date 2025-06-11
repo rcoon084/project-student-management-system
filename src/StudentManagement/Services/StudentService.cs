c4c5f5eb868642d3ba1b4ed87f5dcaf6a2d89104
laurad08412 <lauraagude084@gmail.com>
2025-06-10 23:19:14 -0500

    feat: Add logic to create and store a new student and a new course

--- src/StudentManagement/Services/StudentService.cs
+++ src/StudentManagement/Services/StudentService.cs
@@ -1,1 +1,23 @@
 using StudentManagement.Models;
+
+namespace StudentManagement.Services;
+
+public class StudentService
+{
+    //Lists for storing data
+    public List<Student> studentList = new();
+    public List<Course> courseList = new();
+    public List<Enrollment> enrollmentList = new();
+
+    public void CreateStudent(string firstName, string lastName, string id)
+    {
+        var newStudent = new Student(firstName, lastName, id);
+        studentList.Add(newStudent);
+    }
+
+    public void CreateCourse(string courseId, string courseName, int credits)
+    {
+        var newCourse = new Course(courseId, courseName, credits);
+        courseList.Add(newCourse);
+    }
+}
